67c8c430e045245bf3cb45533a9ba5a9cb9e83d7
elzup <guild0105@gmail.com>
2020-07-13 15:27:27 +0900

    cdr clearn

---
+++ cdr-recent-dirs-clearn.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+cat ~/.chpwd-recent-dirs \
+  | sed -e 's/^..\(.\*\)./\1/g' \
+  | while read line
+do
+if [ -d "$line" ]; then
+echo "\$'\$line'"
+fi
+done
+echo "Success. please reload shell"
